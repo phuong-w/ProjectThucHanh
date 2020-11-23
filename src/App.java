629b932c779284ef62c2d42dfbd3359d712dc802
Nguyễn Thái Phương <73707888+pop0212@users.noreply.github.com>
2020-11-23 11:12:46 +0700

    Update App.java
    dit me may

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("khang ghi");
+        System.out.println("phuong ghi");
     }
 }
